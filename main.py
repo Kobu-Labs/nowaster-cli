9e1164705b9d6ce967819df35e30f1144f9b4325
Kobu <thekobu@gmail.com>
2022-08-21 18:50:16 +0200

    implement simple parsing for time units (#10)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import List, Optional
 
 import requests
@@ -19,16 +19,47 @@ SERVER_ADDR = "http://127.0.0.1:8000"
 __COLORS = list(ANSI_COLOR_NAMES.keys())
 
 
+MIN_HOURS = 0.1
+MIN_MINUTES = 1
+
+
 def get_random_style() -> str:
     return random.choice(__COLORS)
+
+
+def parse_time_unit(unit: str) -> float:
+    if len(unit) <= 1:
+        raise typer.BadParameter("Either number or unit was not passed")
+
+    time_unit = unit[-1]
+    value_raw = unit[: len(unit) - 1]
+
+    try:
+        value = float(value_raw)
+    except ValueError:
+        raise typer.BadParameter("Invalid number format")
+
+    if value < MIN_MINUTES and time_unit == "m":
+        raise typer.BadParameter(f"minimum minutes is {MIN_MINUTES}")
+    if value < MIN_HOURS and time_unit == "h":
+        raise typer.BadParameter(f"minimum hours is {MIN_HOURS}")
+
+    if time_unit == "m":
+        result = timedelta(minutes=value)
+    elif time_unit == "h":
+        result = timedelta(hours=value)
+    else:
+        raise typer.BadParameter("Invalid time unit")
+
+    return result.total_seconds() / 60
 
 
 @app.command()
 def new(
     category: str = typer.Option(..., "--cat", "-c"),
-    duration: int = typer.Option(..., "--dur", "-d"),
-    description: Optional[str] = "",
+    duration_raw: str = typer.Option(..., "--dur", "-d"), description: Optional[str] = None,
 ) -> None:
+    duration = parse_time_unit(duration_raw)
     response = requests.post(
         SERVER_ADDR + "/entries",
         json={"category": category, "duration": duration, "description": description},
@@ -48,7 +79,7 @@ def all(show_ids: bool = typer.Option(False, "--id")) -> None:
     table = Table()
     if show_ids:
         table.add_column("ID")
-    for column in ("Category", "Duration", "Start", "End"):
+    for column in ("Category", "Duration(mins)", "Start", "End"):
         table.add_column(column)
 
     category_colors = {}
